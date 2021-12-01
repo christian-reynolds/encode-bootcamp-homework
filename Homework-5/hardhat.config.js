db0672515962e03d768c9f66dc018c0367eafa43
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-01 08:45:52 -0500

    Added homework 5 to the repo

---
+++ Homework-5/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+require('solidity-coverage');
+let secret = require("./secret")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.10",
+  
+};
