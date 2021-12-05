a66ce58c31fe9a0be50b10a19baf1ada3edd5ac9
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-05 12:19:04 -0500

    Added the function updatePaymentDetails

--- Homework-10/contracts/VolcanoCoin.sol
+++ Homework-10/contracts/VolcanoCoin.sol
@@ -10,6 +10,7 @@ contract VolcanoCoin is ERC20("Volcano Coin", "VLC"), Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _paymentIds;
     enum PaymentType { UNKNOWN, BASIC, REFUND, DIVIDEND, GROUP }
+    uint8 private paymentTypeEnumCount = 4;
     PaymentType constant defaultPaymentType = PaymentType.UNKNOWN;
     
     struct Payment {
@@ -40,15 +41,37 @@ contract VolcanoCoin is ERC20("Volcano Coin", "VLC"), Ownable {
     
     function transfer(address _recipient, uint256 _amount) public virtual override returns (bool) {
         _paymentIds.increment();
-        
+
         _transfer(msg.sender, _recipient, _amount);
         recordPayment(msg.sender, _recipient, _amount);
         return true;
     }
     
     function recordPayment(address _sender, address _recipient, uint256 _amount) internal {
-        //TODO: replace the hardcoded paymentId of 1
         payments[_sender].push(Payment(_paymentIds.current(), _amount, _recipient, defaultPaymentType, "", block.timestamp));
+    }
+
+    function updatePaymentDetails(uint256 _paymentId, uint8 _paymentType, string memory _comment) public {
+        require(_paymentType <= paymentTypeEnumCount, "VolcanoCoin: PaymentType is not a valid value");
+        Payment[] storage addressPayments = payments[msg.sender];
+        require(addressPayments.length > 0, "VolcanoCoin: No payments found for this address");
+        bool paymentFound = false;
+
+        for (uint i=0; i< addressPayments.length; i++) {
+             Payment memory payment = addressPayments[i];
+
+             if (_paymentId == payment.paymentId) {
+                paymentFound = true;
+                payment.paymentType = PaymentType(_paymentType);
+                payment.comment = _comment;
+                addressPayments[i] = payment;
+                 break;
+             }
+        }
+
+        require(paymentFound, "VolcanoCoin: Payment not found");
+
+        payments[msg.sender] = addressPayments;
     }
     
 }
