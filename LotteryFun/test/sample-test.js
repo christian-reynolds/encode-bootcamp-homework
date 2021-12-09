d9a7416b36aa7ddafe12f01c0902359215be918f
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-09 07:12:23 -0500

    Save some tests

--- LotteryFun/test/sample-test.js
+++ LotteryFun/test/sample-test.js
@@ -2,18 +2,26 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  it("", async function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  });
+  it("Attaches to an external contract", async function () {
+    const myContract = await (await ethers.getContractFactory("Lottery")).attach("0x90649B117656e54aB4F2592c1E83e7145Eae1290");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // Do whatever you need with the external contract
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  });
+  it("Attaches gets data from storage at an external contract", async function () {
+    for (let x = 0; x < 50; x++) {
+      const test = await ethers.provider.getStorageAt("0x90649B117656e54aB4F2592c1E83e7145Eae1290", x);
+      console.log("This is my test: ", test);
+    }
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // Do whatever you need with the external contract
+
+  });
+  it("Gets the balance of an address", async function () {
+    const balance0ETH = await ethers.provider.getBalance("0xF803aC944814374a8664B138E280526a2cEEa95b");
+    console.log("This is the test value: ", ethers.utils.formatEther(balance0ETH));
   });
 });
