9050554719aa00a0f23f7c25de746aa2b3f22644
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-10 06:47:26 -0500

    Added a comment to Level 3

--- Homework-11/contracts/Level_3_Global_Functions_Answer.sol
+++ Homework-11/contracts/Level_3_Global_Functions_Answer.sol
@@ -8,6 +8,7 @@ contract Level_3_Global_Functions_Answer {
         gfContract = Level_3_Global_Functions(_gfContractAddress);
     }
 
+    // Once this function is called, you need to wait 256 blocks before calling levelComplete()
     function guess() public {
         gfContract.guessTheFutureHash('');
     }
