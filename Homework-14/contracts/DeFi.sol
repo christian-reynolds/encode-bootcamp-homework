84b54eab636aa6a4d90fa9eadeb9e3d53d99b3ce
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-05 14:05:58 -0500

    Added the getEthUsdPrice function

--- Homework-14/contracts/DeFi.sol
+++ Homework-14/contracts/DeFi.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "./AggregatorV3Interface.sol";
 
 interface Erc20 {
     function approve(address, uint256) external returns (bool);
@@ -20,16 +21,24 @@ interface CErc20 {
 contract DeFi {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant CDAI = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
+    address public constant ETHPriceContract = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
     Erc20 daiContract;
     CErc20 cDAIContract;
+    AggregatorV3Interface ethAggregator;
 
     constructor() {
         daiContract = Erc20(DAI);
         cDAIContract = CErc20(CDAI);
+        ethAggregator = AggregatorV3Interface(ETHPriceContract);
     }
 
     function addToCompound(uint256 amount) public {
         daiContract.approve(CDAI, amount);
         cDAIContract.mint(amount);
+    }
+
+    function getEthUsdPrice() public view returns(int256 price) {        
+        (,price,,,) = ethAggregator.latestRoundData();
+        return price;
     }
 }
